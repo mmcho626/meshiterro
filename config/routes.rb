d900e9265bd5e4ffcadad2aa96970c823321b45a
mmcho626 <office_hagi@yahoo.co.jp>
2019-09-17 06:03:03 +0000

    [Finish 5-5]

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-resources :post_images, only: [:new, :create, :index, :show] do
-resource :post_comments, only: [:create, :destroy]
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
 
-resource :favorites, only: [:create, :destroy]
+ resource :post_comments, only: [:create, :destroy]
+
+ resource :favorites, only: [:create, :destroy]
 
 end
+
+resources :users, only: [:show]
 
 
 end
